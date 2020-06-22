963c7abf82efbc2bb40ceca288c44be3f3d0a619
fmflores <frankthetank67@gmail.com>
2020-06-22 14:21:59 -0700

    Updates

---
+++ precache-manifest.c3da7caa883166edb4ac009d03f38c84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1988b3ca66de4cc10456b0a932be0708",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "9ac67e8f22ab4e128685",
+    "url": "/Clicky-Game/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "094dc2bdccf2deeb7427",
+    "url": "/Clicky-Game/static/js/2.e2f82d03.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Clicky-Game/static/js/2.e2f82d03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ac67e8f22ab4e128685",
+    "url": "/Clicky-Game/static/js/main.970f850f.chunk.js"
+  },
+  {
+    "revision": "79776d704002601449fa",
+    "url": "/Clicky-Game/static/js/runtime-main.758e6d5e.js"
+  }
+]);
