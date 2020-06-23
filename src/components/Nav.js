84d5a66a5aecf138dca771e08dfb68f997e7685f
fmflores <frankthetank67@gmail.com>
2020-06-22 17:58:50 -0700

    fixed route for home logo

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default ({navState}) => <>
 <nav className="navbar navbar-expand-lg navbar-light bg-success" style={{justifyContent:"space-around",boxShadow:"0 0 10px 0"}}>
-  <a className="navbar-brand" href="/">CLICKY GAME</a>
+  <a className="navbar-brand" href="./">CLICKY GAME</a>
       <span className="nav-item active">
             <h4>{navState.msg}</h4>
       </span>
